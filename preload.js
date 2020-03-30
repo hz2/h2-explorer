a648a6d8e3769ba4b9a52aa4537979d239d9879e
huc <ht@live.se>
2020-03-31 00:16:46 +0800

    完成功能原型

--- preload.js
+++ preload.js
@@ -1,17 +1,74 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+const fs = require('fs');
+
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
   }
-console.log(process);
+  console.log(process);
 
   for (const type of ['chrome', 'node', 'electron', 'v8']) {
 
     replaceText(`${type}-version`, process.versions[type])
   }
 
+
+
+
+  // read file dir
+
+
+  let url = document.querySelector('#uri')
+  let val = url && url.value
+
+  async function show(path) {
+    const dir = await fs.promises.opendir(path);
+    for await (const dirent of dir) {
+      console.log(dirent.name, dirent);
+      let dom = document.createElement('div')
+      dom.className = 'item'
+
+      let thumb = ''
+      if (dirent.isDirectory()) {
+        thumb = `<span>目录</span>`
+        dom.ondblclick = () => nextPage(path + '\\' + dirent.name)
+
+      } else if (/\.(png|svg|ico|jpg|jpeg)$/i.test(dirent.name)) {
+        thumb = `<img src="${path}\\${dirent.name}">`
+
+      } else if (/\.(mp4|avi)$/i.test(dirent.name)) {
+        thumb = `<video src="${path}\\${dirent.name}"></video>`
+
+      }
+      dom.innerHTML = `
+      <div class="icon">
+        ${thumb}
+      </div>
+      <div class="text">${dirent.name}</div>
+      `
+      document.querySelector('#result').appendChild(dom)
+    }
+  }
+
+  show(val).catch(console.error);
+
+
+  document.querySelector('#getResult').onclick = () => {
+    let url = document.querySelector('#uri')
+    let val = url && url.value
+
+    document.querySelector('#result').innerHTML = ''
+    show(val).catch(console.error);
+  }
+  nextPage = (val) => {
+    let url = document.querySelector('#uri')
+    url.value = val
+    document.querySelector('#result').innerHTML = ''
+    show(val).catch(console.error);
+  }
   // const btn = document.querySelector('#btn');
   // const path = require('path');
   // const BrowerWindow = require('electron').remote.BrowserWindow;
