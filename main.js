67f53fd2359cb776e39a5efeffbd0810f6e49507
huc <ht@live.se>
2023-06-01 22:48:34 +0800

    update dependencies

--- main.js
+++ main.js
@@ -36,7 +36,9 @@ function createWindow () {
     icon: __dirname + '/src/h2assets/logos/logo-v1.ico',
     backgroundColor: '#f5faff',
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true, // like here
+      webSecurity: false
     }
   })
   // and load the index.html of the app.
@@ -60,20 +62,20 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.whenReady().then(() => {
+  createWindow()
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 // All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-
+// It has the same sandbox as a Chrome extension. 
+const path = require('path')
 
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
@@ -9,39 +9,34 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   console.log(process);
 
-  for (const type of ['chrome', 'node', 'electron', 'v8']) {
-
-    replaceText(`${type}-version`, process.versions[type])
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
   }
+  require(  './modules/readfile.js' ) 
 
+  // const BrowerWindow = require('electron').remote.BrowserWindow;
+  // window.newWindow = path => {
+  //   win = new BrowerWindow({
+  //     width: 960,
+  //     height: 640,
+  //     frame: true, // false隐藏关闭按钮、菜单选项 true显示
+  //     fullscreen: false, // 全屏展示
+  //   })
 
-
-
-  require('./modules/readfile.js')
-
-  const BrowerWindow = require('electron').remote.BrowserWindow;
-  window.newWindow = path => {
-    win = new BrowerWindow({
-      width: 960,
-      height: 640,
-      frame: true, // false隐藏关闭按钮、菜单选项 true显示
-      fullscreen: false, // 全屏展示
-    })
-
-    // win.webContents.openDevTools()
-    win.removeMenu()
-    win.loadURL(path );
-    win.on('close', () => {
-      win = null
-    });
-  }
+  //   // win.webContents.openDevTools()
+  //   win.removeMenu()
+  //   win.loadURL(path );
+  //   win.on('close', () => {
+  //     win = null
+  //   });
+  // }
 
 
 
 
   // 创建菜单  
 
-  require('./modules/menu.js')
+  // require('./modules/menu.js')
   // 引入菜单模块
   // require('./modules/ipcMain.js')
 
